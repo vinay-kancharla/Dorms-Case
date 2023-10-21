bd2f117b90f8a2c572af218810c879c05b858e99
Jake Huber <jah383@case.edu>
2023-10-21 11:20:14 -0400

    initial commit

---
+++ src/main/java/com/dormsatcase/dormsatcase/DormsatcaseApplication.java
@@ -0,0 +1,13 @@
+package com.dormsatcase.dormsatcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormsatcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormsatcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dormsatcase/dormsatcase/DormsatcaseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dormsatcase.dormsatcase;
+
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormsatcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
