974d94727118ebe76fe22a17760a4a841343fc98
root <root@LAPTOP-KVO3A7PR.localdomain>
2023-10-23 23:18:36 -0400

    Added skeleton of remainder Review API requests

--- src/main/java/com/dormsatcase/dormsatcase/review/ReviewController.java
+++ src/main/java/com/dormsatcase/dormsatcase/review/ReviewController.java
@@ -1,6 +1,8 @@
 package com.dormsatcase.dormsatcase.review;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/review")
@@ -19,8 +21,25 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping("/upload")
-    public ResponseEntity<ReviewDTO> upload(@RequestBody ReviewDTO reviewDTO) {
+    public ResponseEntity<Void> upload(@RequestBody ReviewDTO reviewDTO) {
         return reviewService.upload(reviewDTO);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> delete(UUID reviewIdentifier) {
+        return reviewService.delete(reviewIdentifier);
+    }
+
+    @GetMapping("/add-like")
+    public ResponseEntity<Void> addLike(@RequestParam("reviewIdentifier") UUID reviewIdentifier,
+                                        @RequestParam("userIdentifier") UUID userIdentifier) {
+        return reviewService.addLike(reviewIdentifier, userIdentifier);
+    }
+
+    @GetMapping("/add-dislike")
+    public ResponseEntity<Void> addDislike(@RequestParam("reviewIdentifier") UUID reviewIdentifier,
+                                           @RequestParam("userIdentifier") UUID userIdentifier) {
+        return reviewService.addDislike(reviewIdentifier, userIdentifier);
     }
 
 
--- src/main/java/com/dormsatcase/dormsatcase/review/ReviewService.java
+++ src/main/java/com/dormsatcase/dormsatcase/review/ReviewService.java
@@ -4,11 +4,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class ReviewService {
 
-    public ResponseEntity<ReviewDTO> upload(ReviewDTO reviewDTO) {
-        return ResponseEntity.ok(reviewDTO);
+    public ResponseEntity<Void> upload(ReviewDTO reviewDTO) {
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Void> delete(UUID reviewIdentifier) {
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Void> addLike(UUID reviewIdentifier, UUID userIdentifier) {
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<Void> addDislike(UUID reviewIdentifier, UUID userIdentifier) {
+        return ResponseEntity.ok().build();
     }
 
 }
