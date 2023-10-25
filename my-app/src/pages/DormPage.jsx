7a56fcdbc36e847808f8257fa85d6d0ae87d28f4
Nikki D'Costa <nikkidcosta@gmail.com>
2023-10-24 20:08:26 -0400

    added UI for individual dorm pages

--- my-app/src/pages/DormPage.jsx
+++ my-app/src/pages/DormPage.jsx
@@ -1,9 +1,15 @@
 import { useParams } from 'react-router-dom';
+import DormPages from '../components/DormPages.js';
 import NavigationBar from '../components/NavigationBar';
 export default function DormPage() {
     const { dormId } = useParams();
+    const h1Style = {
+        paddingTop:'20px',
+        textAlign: 'center',
+        };
     return <div>
         <NavigationBar/>
-        DORM IS : {dormId} 
+        <h1 class = "display-4" style = {h1Style}>{dormId}</h1>
+        <DormPages/>
         </div>;
   }
