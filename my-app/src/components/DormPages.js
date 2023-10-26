84fecd234ec28627d679abc84c9883515f9b0a69
Nikki D'Costa <nikkidcosta@gmail.com>
2023-10-25 20:55:43 -0400

    added add a review modal

--- my-app/src/components/DormPages.js
+++ my-app/src/components/DormPages.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
+import StarRatings from 'react-star-ratings';
+
 
 const DormPages = () => {
+
+  const [show, setShow] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [textReview, setTextReview] = useState('');
+
+  const closeModal = () => setShow(false);
+  const showModal = () => setShow(true);
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+  };
+
+  const handleReviewTextChange = (e) => {
+    setTextReview(e.target.value);
+  };
+
+  const submitReview = () => {
+    closeModal();
+  };
+
   const containerStyle = {
     display: 'flex',
     flexDirection: 'column',
@@ -32,7 +55,42 @@ const DormPages = () => {
 
   return (
     <div style={containerStyle}>
-     <div style={buttonContainerStyle}><button className="btn btn-danger">Add a Review</button> </div>
+     <div style={buttonContainerStyle}><button className="btn btn-danger" onClick={showModal} >Add a Review</button> 
+      <Modal show={show} onHide={closeModal}>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="rating">
+              <Form.Label>Star Rating</Form.Label>
+              <StarRatings
+                rating={rating}
+                starRatedColor="gold"
+                changeRating={handleRatingChange}
+                numberOfStars={5}
+                starDimension="30px"
+              />
+            </Form.Group>
+            <Form.Group controlId="textReview">
+              <Form.Label>Review: </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={4}
+                value={textReview}
+                onChange={handleReviewTextChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={closeModal}>
+            Upload Image
+          </Button>
+          <Button variant="success" onClick={submitReview}>
+            Validate and Publish
+          </Button>
+        </Modal.Footer>
+      </Modal>
+     </div>
+
       <div style={barStyle1}></div>
       <div style={barStyle2}></div>
     </div>
@@ -40,5 +98,3 @@ const DormPages = () => {
 };
 
 export default DormPages;
-
-  
