d93cc564a50947c483fae4c9dbc8b659282de4e3
Abhimanyu Bangaru <abhi.bangaru@nobel-systems.com>
2023-11-11 12:45:12 -0500

    Made calls to database(dummy because not set up yet)
    Incorporated toastr. Sign up modal made. Better integration usign callbacks.

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -4,12 +4,13 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { toast } from 'react-toastify';
 
 const LoginModal = (props) => {
 
   const [login] = useState(props.login)
 
-  const [loginValue, setLoginValue] = useState("")
+  const [emailValue, setEmailValue] = useState("")
   const [passwordValue, setPasswordValue] = useState("")
 
   const [loginValidator, setLoginValidator] = useState('');
@@ -23,25 +24,68 @@ const LoginModal = (props) => {
   let validate = () => {
     let hasError = false;
     
-    if (loginValue == '') {
-        setLoginValidator("Login value must be filled in!")
+    if (emailValue === '') {
+        setLoginValidator("Email value must be filled in!")
         setLoginError(true);
         hasError = true;
     }
 
-    if (passwordValue == '') {
+    if (passwordValue === '') {
         setPasswordValidator("Password value must be filled in!")
         setPasswordError(true);
         hasError = true;
     }
 
     if (!hasError) {
+        // props.parentCallback({
+        //     action: 'submitted'
+        // });
+        handleSubmit()
         props.parentCallback({
-            action: 'submitted'
-        });
+          action: "account login successful"
+      })
     }
 }
 
+const handleSubmit = async () => {
+  
+  const accountLogin = { emailValue, passwordValue};
+
+  try {
+    const response = await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(accountLogin),
+    });
+
+    if (response.ok) {
+      console.log('Account login successfully');
+      toast.success('Account login successful!');
+      props.parentCallback({
+          action: "account login successful"
+      })
+      
+    } else {
+      console.log('Failed to login account');
+      toast.error('Account login UNSUCCESSFUL');
+      props.parentCallback({
+          action: "account login unsuccessful"
+      })
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+
+  let signupClicked = () => {
+    props.parentCallback({
+      action: "signup"
+    })
+  }
+  
     let cancel = () => {
         props.parentCallback({
             action: 'cancelled'
@@ -50,15 +94,11 @@ const LoginModal = (props) => {
   return (
     <div>
       {/*Dialog*/}
-      {login ? (
+      {
         <DialogTitle sx={{ bgcolor: 'primary.light', color: 'white' }}>
           &nbsp; Login
         </DialogTitle>
-      ) : (
-        <DialogTitle sx={{ bgcolor: 'primary.light', color: 'white' }}>
-         &nbsp; Sign Up
-        </DialogTitle>
-      )}
+     }
       <form>
         <DialogContent>
           {/*input fields for title, description, deadline, and priority*/}
@@ -69,8 +109,8 @@ const LoginModal = (props) => {
               fullWidth
               error={loginError}
               helperText={loginValidator}
-              value={loginValue}
-              onChange={(e) => setLoginValue(e.target.value)}
+              value={emailValue}
+              onChange={(e) => setEmailValue(e.target.value)}
             />
           )}
           <br /> <br />
@@ -89,21 +129,31 @@ const LoginModal = (props) => {
           <br /> <br />
 
         </DialogContent>
-        <DialogActions>
-            <Button
+        <DialogActions style={{ flexDirection: 'column', alignItems: 'flex-end', width: '100%'}}>
+          <Button
+              onClick={signupClicked}
+              variant="contained"
+              sx={{ bgcolor: 'primary.light' , width: '50%'}}
+            >
+              &nbsp;Sign Up!
+            </Button>
+            <br /> 
+          <div style={{ width: '100%', display: 'flex' }}>
+          <Button
               onClick={validate}
               variant="contained"
-              sx={{ bgcolor: 'primary.light', width: '50%' }}
+              sx={{ bgcolor: 'primary.light',width: '50%'}}
             >
               &nbsp;Login
             </Button>
             <Button
               onClick={cancel}
               variant="contained"
-              sx={{ bgcolor: 'red', width: '50%' }}
+              sx={{ bgcolor: 'red',width: '50%' }}
             >
               &nbsp;Cancel
             </Button>
+          </div>
         </DialogActions>
       </form>
     </div>
---
+++ client/src/components/SignupModal.js
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { toast } from 'react-toastify';
+
+const SignupModal = (props) => {
+
+  const [signup] = useState(props.singup)
+
+  const [emailValue, setEmailValue] = useState("")
+  const [passwordValue, setPasswordValue] = useState("")
+  const [passwordConfirmValue, SetPasswordConfirmValue] = useState("");
+  const [emailValidator, setEmailValidator] = useState('');
+  const [passwordValidator, setPasswordValidator] = useState('');
+  const [passwordConfirmValidator, setPasswordConfirmValidator] = useState('');
+
+  
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordConfirmError, setPasswordConfirmError] = useState(false);
+
+  // when hit validate button
+  let validate = () => {
+    let hasError = false;
+    
+    if (emailValue == '') {
+        setEmailValidator("Email value must be filled in!")
+        setEmailError(true);
+        hasError = true;
+    }
+
+    if (passwordValue == '') {
+        setPasswordValidator("Password value must be filled in!")
+        setPasswordError(true);
+        hasError = true;
+    } else if (passwordValue != passwordConfirmValue) {
+        setPasswordValidator("Password value must match with confirmed in!")
+        setPasswordConfirmValidator("Password value must match with confirmed in!")
+        setPasswordError(true);
+        setPasswordConfirmError(true);
+        hasError = true;
+    }
+
+    // if (!hasError) {
+    //     props.parentCallback({
+    //         action: 'submitted'
+    //     });
+    // }
+    if (!hasError){
+        // handleSubmit()
+        props.parentCallback({
+            action: "account creation successful"
+        })
+    }  
+}
+
+const handleSubmit = async () => {
+  
+    const accountToBeCreated = { emailValue, passwordValue};
+  
+    try {
+      const response = await fetch('http://localhost:8080/create-account', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(accountToBeCreated),
+      });
+  
+      if (response.ok) {
+        console.log('Account created successfully');
+        toast.success('Account creation successful!');
+        props.parentCallback({
+            action: "account creation successful"
+        })
+        
+      } else {
+        console.log('Failed to create account');
+        toast.error('Account creation UNSUCCESSFUL');
+        props.parentCallback({
+            action: "account creation unsuccessful"
+        })
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+    let cancel = () => {
+        props.parentCallback({
+            action: 'cancelled'
+        });
+    }
+  return (
+    <div>
+      {/*Dialog*/}
+      {
+        <DialogTitle sx={{ bgcolor: 'primary.light', color: 'white' }}>
+         &nbsp; Sign Up
+        </DialogTitle>
+      }
+      <form>
+        <DialogContent>
+          {/*input fields for title, description, deadline, and priority*/}
+          {(
+            <TextField
+              label="Email"
+              placeholder="Email"
+              fullWidth
+              error={emailError}
+              helperText={emailValidator}
+              value={emailValue}
+              onChange={(e) => setEmailValue(e.target.value)}
+            />
+          )}
+          <br /> <br />
+          {/*Textfield for Description box*/}
+          <TextField
+            aria-label="minimum height"
+            minRows={3}
+            label="Password"
+            placeholder="Password"
+            error={passwordError}
+            helperText={passwordValidator}
+            fullWidth
+            value={passwordValue}
+            onChange={(e) => setPasswordValue(e.target.value)}
+          />
+          <br /> <br />
+          <TextField
+            aria-label="minimum height"
+            minRows={3}
+            label="confirm password"
+            placeholder="Confirm password"
+            error={passwordConfirmError}
+            helperText={passwordConfirmValidator}
+            fullWidth
+            value={passwordConfirmValue}
+            onChange={(e) => SetPasswordConfirmValue(e.target.value)}
+          />
+          <br /> <br />
+
+        </DialogContent>
+        <DialogActions>
+            <Button
+              onClick={validate}
+              variant="contained"
+              sx={{ bgcolor: 'primary.light', width: '50%' }}
+            >
+              &nbsp;Sign Up
+            </Button>
+            <Button
+              onClick={cancel}
+              variant="contained"
+              sx={{ bgcolor: 'red', width: '50%' }}
+            >
+              &nbsp;Cancel
+            </Button>
+        </DialogActions>
+      </form>
+    </div>
+  );
+}
+
+export default SignupModal;
