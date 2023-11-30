bf24d240477c706b74f5c9d90cb723df45129442
Nikki D'Costa <nikkidcosta@gmail.com>
2023-11-29 23:30:10 -0500

    added rankings for home page and experience page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
+import React from 'react';
 import NavigationBar from "../components/NavigationBar.js";
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import PodiumGraph from "../components/Podium.jsx"; 
+import firstyearrankings from "../dummy_data/firstyearrankings.json";
+import secondyearrankings from "../dummy_data/secondyearrankings.json";
+import upperclassrankings from "../dummy_data/upperclassrankings.json";
 
 function HomePage() {
   return (
-      <div>
-          <NavigationBar/>
+    <div>
+      <NavigationBar />
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <PodiumGraph data={firstyearrankings} title="First-Year" />
+      <div></div>
+      <PodiumGraph data={secondyearrankings} title="Second-Year" />
+      <div></div>
+      <PodiumGraph data={upperclassrankings} title="Upperclassmen" />
       </div>
+    </div>
   );
 }
 
